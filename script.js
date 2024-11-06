2d169fe27f663b9a1ed147ed64582905eec8d846
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2024-11-06 12:48:07 +0530

    WebClone

---
+++ script.js
@@ -0,0 +1,24 @@
+// var main=document.querySelector("#page4")
+// var cursor=document.querySelector("#cursor img")
+
+//     main.addEventListener("mouseenter", function () {
+
+//         gsap.to(cursor, {
+//             opacity: 1,
+//             scale: 1
+//         })
+//     })
+//     main.addEventListener("mouseleave", function () {
+//         gsap.to(cursor, {
+//             opacity: 0,
+//             scale: 0
+//         })
+//     })
+
+// main.addEventListener("mousemove",function(dets){
+//     gsap.to("#cursor",{
+//         x: dets.x - main.getBoundingClientRect().x - 70,
+//         y: dets.y - main.getBoundingClientRect().y - 30
+//         // duration:0.8
+//     })
+// })
